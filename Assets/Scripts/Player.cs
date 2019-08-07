3afe434bc8ed95a06d916b1a14252a3c3d858e26
pokrov <al.n.pokrovsky@gmail.com>
2019-08-07 10:30:59 +0300

    player borders

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public BoxCollider Boundary;
+
+    private SpaceShip ship;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ship = GameObject.Find("SpaceShip").GetComponent<SpaceShip>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xMove = Input.GetAxis("Horizontal");
+        float yMove = Input.GetAxis("Vertical");
+        ship.Move(xMove, yMove, Boundary);
+        if (Input.GetButton("Fire1")) {
+            ship.ShotMainGun();
+        }
+    }
+}
--- Assets/Scripts/SpaceShip.cs
+++ Assets/Scripts/SpaceShip.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,33 +8,54 @@ public class SpaceShip : MonoBehaviour
     public float MotionSpeed = 1;
     public float Tilt = 1;
     public GameObject Сartridge;
-    public Transform BigGun;
+    public Transform MainGun;
+
+    public float ReloadMainGunTimeout = 1;
 
     private Rigidbody body;
+    private SphereCollider sphere;
+    private float reloadMainGunTime;
+    private bool ReloadMainGun {
+        get { return Time.time < reloadMainGunTime; }
+        set { 
+            if (value == true) {
+                reloadMainGunTime = Time.time + ReloadMainGunTimeout;
+            }
+        }
+    }
+
+    public void ShotMainGun() {
+        if (!ReloadMainGun) {
+            ReloadMainGun = true;
+            Instantiate(Сartridge, MainGun.position, body.rotation);
+        }
+    }
+    public void Move(float xMove, float yMove, BoxCollider boundary = null) {
+        body.velocity = new Vector3(xMove, yMove, 0) * MotionSpeed;
+        body.rotation = Quaternion.Euler(yMove*Tilt, -xMove*Tilt, 0);
+        if (boundary != null) {
+            Vector3 center = boundary.center - sphere.center;
+            float xPos = Mathf.Clamp(body.position.x,
+                                center.x - (boundary.size.x/2-sphere.radius),
+                                center.x + (boundary.size.x/2-sphere.radius));
+            float yPos = Mathf.Clamp(body.position.y, 
+                                center.y - (boundary.size.y/2-sphere.radius),
+                                center.y + (boundary.size.y/2-sphere.radius));
+            body.position = new Vector3(xPos, yPos, body.position.z);
+        }
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-        body = GetComponent<Rigidbody>();   
+        body = GetComponent<Rigidbody>();
+        sphere = GetComponent<SphereCollider>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        UserUpdatePosition();
-        UserShot();
+        
     }
 
-    private void UserUpdatePosition() {
-        float xMove = Input.GetAxis("Horizontal");
-        float yMove = Input.GetAxis("Vertical");
-        body.velocity = new Vector3(xMove, yMove, 0) * MotionSpeed;
-        body.rotation = Quaternion.Euler(yMove*Tilt, -xMove*Tilt, 0);
-    }
-
-    private void UserShot() {
-        if (Input.GetButton("Fire1")) {
-            Instantiate(Сartridge, BigGun.position, Quaternion.identity);
-        }
-    }
 }
