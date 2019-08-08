3f2a06201d0d6da78333ad0a1992b10dd2bc7ecc
pokrov <al.n.pokrovsky@gmail.com>
2019-08-08 17:32:40 +0300

    move explosions to it's folder and make autodestroy

---
+++ Assets/Scripts/AutodestroyScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutodestroyScript : MonoBehaviour
+{
+    public float Timeout = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(this.gameObject, Timeout);
+    }
+}
