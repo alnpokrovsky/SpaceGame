821d2b8f4ffacb8914f7e45f562ced38ac43398f
pokrov <al.n.pokrovsky@gmail.com>
2019-08-09 16:20:01 +0300

    randomize asteroids

--- Assets/Scripts/EnemyFactoryScript.cs
+++ Assets/Scripts/EnemyFactoryScript.cs
@@ -1,18 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class EnemyFactoryScript : MonoBehaviour
 {
-    public GameObject Asteroid;
-    public GameObject SpaceShip;
-    public float Timeout = 1;
+    public GameObject[] Asteroids;
+    public GameObject[] SpaceShips;
+    public float AsteroidsProbability = 0.8f;
+    public int EnemiesCount = 20;
+    public float EnemiesTimeout = 0.8f;
+    public float WavesTimeout = 5;
 
     private float minXPos;
     private float maxXPos;
     private float yPos;
     private float zPos;
-    private Reloader reloader;
 
     void Awake()
     {
@@ -20,22 +23,34 @@ public class EnemyFactoryScript : MonoBehaviour
         maxXPos = transform.position.x + transform.localScale.x/2;
         yPos = transform.position.y;
         zPos = transform.position.z;
-
-        reloader = new Reloader(Timeout);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (reloader.Ready) {
-            reloader.Shot();
-            Vector3 p = new Vector3(Random.Range(minXPos, maxXPos), yPos, zPos);
-            if (Random.value > 0.2) {
-                Instantiate(Asteroid, p, Quaternion.identity);
-            } else {
-                Instantiate(SpaceShip, p, Quaternion.identity)
-                .AddComponent(typeof(EnemyScript));
+    void Start() {
+        Random.InitState ((int)System.DateTime.Now.Ticks);
+        StartCoroutine(GenerateWaves());
+    }
+
+    private IEnumerator GenerateWaves() {
+        while (true) {
+            yield return new WaitForSeconds(WavesTimeout);
+            foreach (int i in Enumerable.Range(0, EnemiesCount))
+            {
+                Vector3 p = new Vector3(Random.Range(minXPos, maxXPos), yPos, zPos);
+                GenerateEnemy(p);
+                yield return new WaitForSeconds(EnemiesTimeout);
             }
+        }
+    }
+
+    private GameObject GenerateEnemy(Vector3 p) {
+        if (Random.value < AsteroidsProbability) {
+            int asteroidType = Random.Range(0, Asteroids.Length);
+            return Instantiate(Asteroids[asteroidType], p, Quaternion.identity);
+        } else {
+            int shipType = Random.Range(0, SpaceShips.Length);
+            GameObject ship = Instantiate(SpaceShips[shipType], p, Quaternion.identity);
+            ship.AddComponent(typeof(EnemyScript));
+            return ship;
         }
     }
 }
