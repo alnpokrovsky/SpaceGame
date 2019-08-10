aa1acda9ab7516937796fd2b05eb8798a91fc902
pokrov <al.n.pokrovsky@gmail.com>
2019-08-10 23:20:48 +0300

    game restart

---
+++ Assets/Scripts/Common/TextAlphaPingPong.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextAlphaPingPong : MonoBehaviour
+{
+    public float Period = 1;
+
+    private Text text;
+
+    void Awake(){
+        text = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        text.color = new Color(
+            text.color.r, text.color.b, text.color.b,
+            Mathf.PingPong(Time.time/Period, 1) 
+        );
+    }
+}
